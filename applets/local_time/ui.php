869de142df03870dc7fbd990cd37ac1d6c1c8a78
John Britton <public@johndbritton.com>
2010-06-22 23:06:26 -0700

    fixing spelling error

--- applets/local_time/ui.php
+++ applets/local_time/ui.php
@@ -1,5 +1,5 @@
 <div class="vbx-applet">
-  <h2 class="settings-title">Dopplr: Loacl Time</h2>
+  <h2 class="settings-title">Dopplr: Local Time</h2>
   <p>This applet will report the travellers local time. <em>"10:56PM"</em></p>
   <h2 class="settings-title">Next</h2>
   <p>After the initial prompt, continue to the next applet</p>
