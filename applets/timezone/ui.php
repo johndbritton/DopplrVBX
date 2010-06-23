846ca04c7cb505288ccdbc32fa48fb12e9bce756
John Britton <public@johndbritton.com>
2010-06-23 12:44:58 -0700

    fixed timezone applet title

--- applets/timezone/ui.php
+++ applets/timezone/ui.php
@@ -1,5 +1,5 @@
 <div class="vbx-applet">
-  <h2 class="settings-title">Dopplr: Local Time</h2>
+  <h2 class="settings-title">Dopplr: Timezone</h2>
   <p>This applet will report the travellers timezone. <em>"America/Los Angeles, U.T.C. Offset: -28800"</em></p>
   <h2 class="settings-title">Next</h2>
   <p>After the initial prompt, continue to the next applet</p>
